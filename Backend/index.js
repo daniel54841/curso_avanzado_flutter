5f4ed8a1eca796d423d406bd7d850978a7cb5e9b
Daniel Pastor Sardá <daniel.pastor@getronics.com>
2023-12-28 14:02:37 +0100

    Video 21
    Configuracion inicial

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
+///App de express
 const app = express(); 
 const path = require("path");
 require('dotenv').config(); //definir las variables de entorno
+
+//crear servidor sockets
+const server = require("http").createServer(app); //usar la configuracion de la app
+const io = require("socket.io")(server); //informacion que entra y sale
+
+// mensajes de sockets
+io.on("connection",client => {
+    console.log("Cliente conectado");
+    client.on("disconnect",() => {
+        console.log("Cliente desconectado");
+    })
+});
+
 const { execPath } = require('process');
 
 
@@ -11,7 +25,7 @@ const publicPath = path.resolve(__dirname, "public");
 app.use(express.static(publicPath));
 
 
-app.listen(process.env.PORT //definir el puerto definido en .env
+server.listen(process.env.PORT //definir el puerto definido en .env
     ,( err ) =>{
     if(err) throw new Error(err);
     console.log('Servidor corriendo en puerto',3000);

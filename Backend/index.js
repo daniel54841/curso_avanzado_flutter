526e5c96e6626044ae9393270c4e3a5dda9cb247
Daniel Pastor Sardá <daniel.pastor@getronics.com>
2023-12-28 13:51:17 +0100

    Video 19,20 y 21
    Directorio publico,variables de entorno y configuracion inicial

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
-
 const app = express(); 
+const path = require("path");
+require('dotenv').config(); //definir las variables de entorno
+const { execPath } = require('process');
 
 
-app.listen(3000,( err ) =>{
+//Path publico
+const publicPath = path.resolve(__dirname, "public");
+
+app.use(express.static(publicPath));
+
+
+app.listen(process.env.PORT //definir el puerto definido en .env
+    ,( err ) =>{
     if(err) throw new Error(err);
     console.log('Servidor corriendo en puerto',3000);
 }); //escuchar en el puerto indicado

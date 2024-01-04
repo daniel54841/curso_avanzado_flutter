dafb1d884561ec3425d7a517e3414c8b9fadf0a1
Daniel Pastor Sardá <daniel.pastor@getronics.com>
2024-01-04 13:30:56 +0100

    User_page
    Video 57

--- lib/chat/page/usuarios_page.dart
+++ lib/chat/page/usuarios_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 import '../models/user.dart';
 
@@ -10,6 +12,24 @@ class UsersPage extends StatefulWidget {
 }
 
 class _UsersPageState extends State<UsersPage> {
+  final RefreshController _refreshCtrl = RefreshController(initialRefresh: false);
+
+  void _onRefresh() async {
+    // monitor network fetch
+    await Future.delayed(const Duration(milliseconds: 1000));
+    // if failed,use refreshFailed()
+    _refreshCtrl.refreshCompleted();
+  }
+
+  void _onLoading() async {
+    // monitor network fetch
+    await Future.delayed(const Duration(milliseconds: 1000));
+    // if failed,use loadFailed(),if no data return,use LoadNodata()
+    debugPrint("Comprobando si hay mas datos en la lista");
+    if (mounted) setState(() {});
+    _refreshCtrl.loadComplete();
+  }
+
   List<User> users = [
     User(online: true, email: "maria@test.com", nombre: "Maria", uid: "1"),
     User(online: false, email: "miguel@test.com", nombre: "Miguel", uid: "2"),
@@ -39,27 +59,63 @@ class _UsersPageState extends State<UsersPage> {
           )
         ],
       ),
-      body: ListView.separated(
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text("${users[index].nombre}"),
-            leading: CircleAvatar(
-              child: Text(users[index].nombre!.substring(0, 2)),
-            ),
-            trailing: Container(
-              width: 10,
-              height: 10,
-              decoration: BoxDecoration(
-                color: users[index].online! ? Colors.green[300] : Colors.red,
-                borderRadius: BorderRadius.circular(100),
+      body: SmartRefresher(
+        enablePullDown: true,
+        enablePullUp: false,
+        header: const TwoLevelHeader(
+          completeIcon: Icon(
+            Icons.check,
+            color: Colors.green,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+          ),
+        ),
+        footer: CustomFooter(
+          builder: (BuildContext context, LoadStatus? mode) {
+            Widget body;
+            if (mode == LoadStatus.idle) {
+              body = const Text("pull up load");
+            } else if (mode == LoadStatus.loading) {
+              body = const CupertinoActivityIndicator();
+            } else if (mode == LoadStatus.failed) {
+              body = const Text("Load Failed!Click retry!");
+            } else if (mode == LoadStatus.canLoading) {
+              body = const Text("release to load more");
+            } else {
+              body = const Text("No more Data");
+            }
+            return SizedBox(
+              height: 55.0,
+              child: Center(child: body),
+            );
+          },
+        ),
+        controller: _refreshCtrl,
+        onRefresh: _onRefresh,
+        onLoading: _onLoading,
+        child: ListView.separated(
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text("${users[index].nombre}"),
+              leading: CircleAvatar(
+                child: Text(users[index].nombre!.substring(0, 2)),
               ),
-            ),
-          );
-        },
-        separatorBuilder: (context, index) {
-          return const Divider();
-        },
-        itemCount: users.length,
+              trailing: Container(
+                width: 10,
+                height: 10,
+                decoration: BoxDecoration(
+                  color: users[index].online! ? Colors.green[300] : Colors.red,
+                  borderRadius: BorderRadius.circular(100),
+                ),
+              ),
+            );
+          },
+          separatorBuilder: (context, index) {
+            return const Divider();
+          },
+          itemCount: users.length,
+        ),
       ),
     );
   }

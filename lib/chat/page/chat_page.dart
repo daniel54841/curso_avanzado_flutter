76fb836ec306f62ab0e8ed9fe05af888168df13d
Daniel Pastor Sardá <daniel.pastor@getronics.com>
2024-01-04 14:33:38 +0100

    Pantalla chat Parte 1
    Video 65

--- lib/chat/page/chat_page.dart
+++ lib/chat/page/chat_page.dart
@@ -1,10 +1,87 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
-  const ChatPage({Key? key}) : super(key: key);
-
+  ChatPage({Key? key}) : super(key: key);
+  final TextEditingController _textCtrl = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        backgroundColor: Colors.white,
+        title: Center(
+          child: Column(
+            children: [
+              CircleAvatar(
+                maxRadius: 14,
+                backgroundColor: Colors.blue[100],
+                child: const Text(
+                  "Te",
+                  style: TextStyle(fontSize: 12),
+                ),
+              ),
+              const SizedBox(
+                height: 3,
+              ),
+              const Text(
+                "Nombre completo",
+                style: TextStyle(color: Colors.black87, fontSize: 15),
+              )
+            ],
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return Text("$index");
+                },
+                reverse: true,
+              ),
+            ),
+            const Divider(
+              height: 1,
+            ),
+            Container(
+              color: Colors.white,
+              height: 50,
+              child: Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 8.0,
+                ),
+                child: Row(
+                  children: [
+                    Flexible(
+                      child: TextField(
+                        controller: _textCtrl,
+                        onSubmitted: (_) {
+                          _textCtrl.clear();
+                          _focusNode.requestFocus();
+                        },
+                        onChanged: (value) {},
+                        decoration: const InputDecoration.collapsed(hintText: "Enviar mensaje"),
+                        focusNode: _focusNode,
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                      child: IconButton(
+                        icon: const Icon(Icons.send_rounded),
+                        onPressed: () {},
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/chat/routes/routes.dart
+++ lib/chat/routes/routes.dart
@@ -8,7 +8,7 @@ import '../page/usuarios_page.dart';
 
 final Map<String, Widget Function(BuildContext)> appRoutes = {
   "users": (_) => const UsersPage(),
-  "chat": (_) => const ChatPage(),
+  "chat": (_) => ChatPage(),
   "login": (_) => const LoginPage(),
   "register": (_) => const RegisterPage(),
   "loading": (_) => const LoadingPage(),

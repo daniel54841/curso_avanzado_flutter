c52551e58d3b95e7638a2979a846b54713c644fe
Daniel Pastor Sardá <daniel.pastor@getronics.com>
2024-01-08 17:17:27 +0100

    Añadir validacion de parametros de crear usuario

--- Backend_chat/controllers/auth.js
+++ Backend_chat/controllers/auth.js
@@ -1,8 +1,13 @@
 const { response } = require("express");
+const { validationResult } = require("express-validator");
 
 
 
 const crearUsuario= (req,res=response) =>{
+    const errores = validationResult( req );
+
+    
+
     res.json({
         ok: true,
         msg:"Crear usuario!!!"
--- Backend_chat/routes/auth.js
+++ Backend_chat/routes/auth.js
@@ -4,9 +4,16 @@
 
 const { Router, response } = require("express");
 const { crearUsuario } = require("../controllers/auth");
+const { check } = require("express-validator");
+const { validarCampos } = require("../middleware/validar-campos");
 const router = Router();
 
-router.post("/new", crearUsuario);
+router.post("/new", [
+  check("nombre","El nombre es obligatorio").not().isEmpty(),
+  check("password","La contraseña es obligatoria").not().isEmpty(),
+  check("email","El email es obligatorio").not().isEmail(),
+  validarCampos
+],crearUsuario);
 
 
 module.exports = router;
